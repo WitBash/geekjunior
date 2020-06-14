d35572a5ab707b363af9ae49423304a9588ae287
wib <vitalij.boshko@gmail.com>
2020-06-14 12:11:31 +0300

    Created builder for Person

--- src/main/java/com/boshko/geekjunior/hw1/Main.java
+++ src/main/java/com/boshko/geekjunior/hw1/Main.java
@@ -1,4 +1,7 @@
 package com.boshko.geekjunior.hw1;
 
 public class Main {
+    public static void main(String[] args) {
+        Person person = new Person.PersonBuilder().addFirstName("").addLastName("").build();
+    }
 }
---
+++ src/main/java/com/boshko/geekjunior/hw1/Person.java
@@ -0,0 +1,68 @@
+package com.boshko.geekjunior.hw1;
+
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private String middleName;
+    private String country;
+    private String address;
+    private String phone;
+    private int age;
+    private String gender;
+
+    public static class PersonBuilder {
+        private String firstName;
+        private String lastName;
+        private String middleName;
+        private String country;
+        private String address;
+        private String phone;
+        private int age;
+        private String gender;
+
+        public PersonBuilder addFirstName (String firstName){
+            this.firstName = firstName;
+            return this;
+        }
+
+        public PersonBuilder addLastName (String lastName){
+            this.lastName = lastName;
+            return this;
+        }
+
+        public PersonBuilder addMiddleName (String middleName){
+            this.middleName = middleName;
+            return this;
+        }
+
+        public PersonBuilder addCountry (String country){
+            this.country = country;
+            return this;
+        }
+
+        public PersonBuilder addAddress (String address){
+            this.address = address;
+            return this;
+        }
+
+        public PersonBuilder addPhone (String phone){
+            this.phone = phone;
+            return this;
+        }
+
+        public PersonBuilder addAge (int age){
+            this.age = age;
+            return this;
+        }
+
+        public PersonBuilder addGender (String gender){
+            this.gender = gender;
+            return this;
+        }
+
+        public Person build(){
+            return new Person();
+        }
+    }
+}
